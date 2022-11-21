e71469dd466a9372bf369c3f60e7e4536c4c7949
nsbn65 <saito12pbyf8o@gmail.com>
2022-11-21 09:48:08 +0900

    advanced-larapj

---
+++ app/Models/Author.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'age', 'nationality'];
+    
+    public function book(){
+    return $this->hasOne('App\Models\Book');
+    }
+    public function books(){
+    return $this->hasMany('App\Models\Book');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+
+    Route::prefix('book')->group(function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/add', [BookController::class, 'add']);
+    Route::post('/add', [BookController::class, 'create']);
+    Route::get('/relation', [AuthorController::class, 'relate']);
+});
